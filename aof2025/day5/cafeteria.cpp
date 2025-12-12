a8a709fad8bed2a38b0faf4070fd00c390f20fb7
demergor <jim.nguy39@gmail.com>
2025-12-12 14:46:31 +0100

    Added solutions to aof2025 day5 part one, still working on part two

--- aof2025/day5/cafeteria.cpp
+++ aof2025/day5/cafeteria.cpp
@@ -2,50 +2,60 @@
 #include <iostream>
 #include <string>
 #include <unordered_set>
+#include <vector>
 
-int num_fresh {0};
+struct Range {
+  long long lowest;
+  long long highest;
+};
 
-void print(const std::unordered_set<int>& us) {
-  for (const auto& val : us) {
-    std::cout << val << '\n';
-  }
-}
-
-void add_range(const std::string& s, std::unordered_set<int>& range) {
+bool fresh_id(const std::string& s, std::vector<Range>& ranges) {
   if (s.empty()) {
-    return;
+    return false;
   }
-
-  print(range);
 
   size_t index {s.find('-')};
   if (std::string::npos == index) {
-    ::num_fresh += range.find(std::stoi(s)) == range.end() ? 0 : 1;
-    return;
+    long long cur {std::stoll(s)};
+    for (const auto& r : ranges) {
+      if (cur >= r.lowest && cur <= r.highest) {
+        return true;
+      }
+    }
+    return false;
   }
 
   assert (index < s.size() - 1);
-  for (int i {std::stoi(s.substr(0, index))}; i <= std::stoi(s.substr(index + 1, s.size())); ++i) {
-    range.insert(i);
+  Range r {std::stoll(s.substr(0, index)), std::stoll(s.substr(index + 1, s.size()))};
+  ranges.push_back(r);
+  return false;
+}
+
+void reduce(std::vector<Range>& ranges) {
+  bool change_made {false};
+  size_t index {0};
+  while (index < ranges.size()) {
+    Range cur {ranges[index]};
+    for (auto it {++ranges.begin()}; it != ranges.end();) {
+      if (*it.lowest >= cur.lowest && *it.lowest <= cur.highest) {
+        ranges[index] = merge(cur, index);
+        break;
+      }
+    }
+    ++index;
   }
 }
 
-
 int main() {
   std::cout << "Please enter your list of ranges and IDs below:\n";
-  std::unordered_set<int> fresh_range;
+  std::vector<Range> ranges;
   std::string input;
-  std::cin >> input;
-  while (!input.empty()) {
-    add_range(input, fresh_range);
-    std::cin >> input;
-  }
   int num_fresh {0};
-  int id {0};
-  while (std::cin >> id) {
-    num_fresh += (fresh_range.find(id) == fresh_range.end()) ? 0 : 1;
+  while (std::getline(std::cin, input)) {
+    num_fresh += fresh_id(input, ranges);
   }
+  reduce(ranges);
   std::cout << "\nNumber of available fresh ingredient IDs part one: " << num_fresh << '\n';
-  print(fresh_range);
+  std::cout << "\nNumber of total fresh ingredient IDs part two: " << 
   return 0;
 }
